67fe37f290b941477db941858ba1079c1881a320
yiwenluiris <93690224+yiwenluiris@users.noreply.github.com>
2024-03-04 18:11:12 -0500

    weighted origami plots

--- origami_run.R
+++ origami_run.R
@@ -21,29 +21,26 @@ df2 <- data_preparation(df2)
 col1 <- c(0.2,0.5,0.5)
 col2 <- c(0.5,0.1,0.1)
 
-par(mfrow = c(1,2))
+par(mfrow = c(1,1))
 
-origami_plot(df=df1, col = col1,
+origami_plot(df=df1, pcol = rgb(0.2,0.5,0.5,1),
+             pfcol = rgb(0.2,0.5,0.5,0.1),
              axistype=1, 
              pty = 32,
              centerzero = TRUE)
 
-origami_plot(df=df2, col = col2,
-             axistype=1, 
-             pty = 32,
-             centerzero = TRUE)
+origami_plot_pairwise(df1=df1, df2=df2, pcol1 = rgb(0.2,0.5,0.5,1), 
+                      pfcol1 = rgb(0.2,0.5,0.5,0.1), 
+                      pcol2 = rgb(0.5,0.1,0.1,1),
+                      #pfcol2 = rgb(0.5,0.1,0.1,0.1),
+                      axistype=1, pty = 32,centerzero = TRUE)
 
-
-par(mfrow=c(1,1))
-origami_plot_pairwise(df1, df2, col1, col2,
+df1 <- read.csv("sample_data_1.csv")
+df1 <- df1[1,1:5]
+df1 <- data_preparation(df1)
+weight <- c(0.25,0.1,0.35,0.2,0.1)
+#weight <- c(0.4,0.2,0.15,0.13,0.12)
+origami_plot_weighted(df1, weight, pcol = rgb(0.2,0.5,0.5,1), pfcol = rgb(0.2,0.5,0.5,0.1),
                       axistype=1, 
                       pty = 32,
                       centerzero = TRUE)
-
-
-test <- area_calculation(df1)
-write.csv(test,"area_1.csv")
-
-test_double <- area_calculation(df1_double)
-write.csv(test_double,"area_2_2.csv")
-
